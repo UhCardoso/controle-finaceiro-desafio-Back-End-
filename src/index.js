e55328158a9a7c6e513d24cd447cfcf5798a0ce6
uh cardoso <uhcardoso@protonmail.com>
2022-05-31 00:41:43 -0300

    adiciona configuracao cors

--- src/index.js
+++ src/index.js
@@ -7,6 +7,19 @@ const app = express();
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: false}));
 
+// Cors config
+app.use((req, res, next) => {
+	// Permitir acesso a outros sites
+    res.header("Access-Control-Allow-Origin", "*");
+	// Permitir metodos HTTP
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: false}));
+
 require('./app/controllers/index')(app);
 
 app.listen(3132);
